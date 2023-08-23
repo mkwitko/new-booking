225fb7941f0f2511ebcf529040df06d0556998b4
saimonFill <saimongabriel@hotmail.com>
2023-08-23 18:28:04 -0300

    feat: create modal search reserves

---
+++ src/app/(logged)/reserves/(sections)/Search/SearchReserves.tsx
@@ -0,0 +1,143 @@
+'use client'
+
+import InputContainer from '@/components/coreComponents/containers/InputContainer'
+import Button from '@/components/interactiveComponents/Button'
+import { B2BCombobox } from '@/components/interactiveComponents/ComboBox'
+import { B2BDatePicker } from '@/components/interactiveComponents/DatePicker'
+import { Button as ButtonUI } from '@/components/ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { LoggedContext } from '@/context/LoggedContext'
+import useSearchHook from '@/hooks/search/Search'
+import { useContext, useState } from 'react'
+import useSearchReservesHook from '@/hooks/reserves/SearchReserves'
+
+export default function SearchReservesComponent() {
+  const { user } = useContext(LoggedContext)
+
+  const [moreFilters, setMoreFilters] = useState<boolean>(false)
+
+  const {
+    salePoint,
+    setSalePoint,
+    city,
+    setCity,
+    checkIn,
+    setCheckIn,
+    checkOut,
+    setCheckOut,
+  } = useSearchHook()
+
+  const {
+    statusList,
+    statusSelected,
+    setStatusSelected,
+    locator,
+    setLocator,
+    dateTypeList,
+    dateType,
+    setDateType,
+    client,
+    setClient,
+  } = useSearchReservesHook()
+
+  const handleInputLocatorChange = (event: any) => {
+    const inputValue = event.target.value
+    const numericValue = inputValue.replace(/[^0-9]/g, '')
+
+    setLocator(numericValue)
+  }
+
+  console.log('locator', locator)
+
+  return (
+    <>
+      <div
+        className="flex w-full flex-col gap-4
+      xl:flex-row"
+      >
+        <InputContainer label="Localizador">
+          <Popover>
+            <PopoverTrigger asChild>
+              {/* trocar o ButtonUI por um componente de input de texto */}
+              <ButtonUI
+                variant="outline"
+                role="combobox"
+                className="w-full justify-between"
+              />
+            </PopoverTrigger>
+          </Popover>
+        </InputContainer>
+
+        <InputContainer label="Ponto de venda">
+          <B2BCombobox
+            options={user?.hook?.data.data}
+            value={salePoint}
+            setValue={setSalePoint}
+            labelTag="corporateName"
+            valueTag="companyId"
+          />
+        </InputContainer>
+
+        <InputContainer label="Status">
+          <B2BCombobox
+            options={''}
+            value={statusSelected}
+            setValue={setStatusSelected}
+          />
+        </InputContainer>
+
+        <InputContainer label="Tipo de Data">
+          <B2BCombobox
+            options={''}
+            value={dateType}
+            setValue={setStatusSelected}
+          />
+        </InputContainer>
+
+        <InputContainer label="Entrada e Saída">
+          <B2BDatePicker />
+        </InputContainer>
+      </div>
+
+      {moreFilters && (
+        <div
+          className="flex w-[50%] flex-col items-center justify-start gap-8
+          lg:flex-row"
+        >
+          <InputContainer label="Cidade">
+            <B2BCombobox options={''} value={city} setValue={setCity} />
+          </InputContainer>
+
+          <InputContainer label="Cliente">
+            <B2BCombobox options={''} value={client} setValue={setClient} />
+          </InputContainer>
+        </div>
+      )}
+
+      <div className="flex w-full items-center justify-end">
+        <div className="flex gap-4 lg:w-1/4">
+          <Button
+            label="Limpar Filtro"
+            textClass="text-textDisabled"
+            color="light"
+            mergeClass="px-0"
+          />
+          <Button label="Buscar" mergeClass="px-0" />
+        </div>
+      </div>
+
+      <div className="flex w-full justify-center border-t-[0.06rem] border-borderColor">
+        <p
+          className="mt-6 cursor-pointer text-xs font-semibold uppercase text-primaryDark"
+          onClick={() => setMoreFilters(!moreFilters)}
+        >
+          {moreFilters ? 'FILTRO SIMPLES' : 'FILTRO AVANÇADO'}
+        </p>
+      </div>
+    </>
+  )
+}
--- src/app/(logged)/reserves/page.tsx
+++ src/app/(logged)/reserves/page.tsx
@@ -1,13 +1,14 @@
 import Container from '@/components/coreComponents/containers/Container'
 import WhiteBox from '@/components/coreComponents/containers/WhiteBox'
 import Title from '@/components/text/Title'
+import SearchReservesComponent from './(sections)/Search/SearchReserves'
 
 export default function Reserves() {
   return (
     <Container>
       <Title title="Busque por reservas" />
       <WhiteBox>
-        <p>aehuirehaiurea</p>
+        <SearchReservesComponent />
       </WhiteBox>
     </Container>
   )
