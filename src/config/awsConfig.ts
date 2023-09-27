8e77c758a7f54f6a7ee51999db0d4ebacf42acd8
mauricio de oliveira kwitko <mauricio.kwitko@ezoom.com.br>
2023-09-27 16:17:57 -0300

    update domain 3

--- src/config/awsConfig.ts
+++ src/config/awsConfig.ts
@@ -26,7 +26,7 @@ export const AWSConfig = {
   //         ? 'localhost'
   //         : '.b2breservas.com.br',
   cookieStorage: {
-    domain: ".new-booking-4puvgmwq5-mkwitko.vercel.app",
+    domain: ".new-booking-git-master-mkwitko.vercel.app",
     path: "/",
     expires: 1,
     secure: true,
@@ -61,7 +61,7 @@ Amplify.configure({
       //     process.env.NODE_ENV === "development"
       //       ? "localhost"
       //       : ".b2breservas.com.br",
-      domain: ".new-booking-4puvgmwq5-mkwitko.vercel.app",
+      domain: ".new-booking-git-master-mkwitko.vercel.app",
       path: "/",
       expires: 1,
       secure: true,
